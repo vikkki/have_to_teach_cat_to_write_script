4c6334a48e830c9aee5d2ea3235619f9245dcaa4
Xiaofan Zhao <zhaoxiaofanjiayou@163.com>
2017-07-10 00:37:55 +0800

    Update wget_ssr.sh

--- wget_ssr.sh
+++ wget_ssr.sh
@@ -1,3 +1,4 @@
 for ((i=0;i<=2;i++)); 
 do b=6;v=`expr $i + $b`;
-wget ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX/SRX180/SRX180130$i/SRR358995$v/SRR358995$v.sra; done
+wget ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX/SRX180/SRX180130$i/SRR358995$v/SRR358995$v.sra;
+done
